65ea00a5a02300dba03774c0ea416e71693df8eb
lazy_owl <slimsly11@gmail.com>
2023-06-11 22:19:28 +0600

    more condition operators

--- internal/builders/select_builder/api.go
+++ internal/builders/select_builder/api.go
@@ -184,16 +184,32 @@ func (builder *Builder) Equal(field string, value any) string {
 	return builder.getFieldItem(field) + " = " + type_helper.ToString(value)
 }
 
+func (builder *Builder) NotEqual(field string, value any) string {
+	return builder.getFieldItem(field) + " != " + type_helper.ToString(value)
+}
+
 func (builder *Builder) In(field string, value any) string {
 	return builder.getFieldItem(field) + " IN " + type_helper.ToString(value)
+}
+
+func (builder *Builder) NotIn(field string, value any) string {
+	return builder.getFieldItem(field) + " NOT IN " + type_helper.ToString(value)
 }
 
 func (builder *Builder) Like(field string, value string) string {
 	return builder.getFieldItem(field) + " LIKE " + type_helper.ToString(value)
 }
 
+func (builder *Builder) NotLike(field string, value string) string {
+	return builder.getFieldItem(field) + " NOT LIKE " + type_helper.ToString(value)
+}
+
 func (builder *Builder) ILike(field string, value string) string {
 	return builder.getFieldItem(field) + " ILIKE " + type_helper.ToString(value)
+}
+
+func (builder *Builder) NotILike(field string, value string) string {
+	return builder.getFieldItem(field) + " NOT ILIKE " + type_helper.ToString(value)
 }
 
 func (builder *Builder) Gte(field string, value any) string {
@@ -210,6 +226,14 @@ func (builder *Builder) Lte(field string, value any) string {
 
 func (builder *Builder) Lt(field string, value any) string {
 	return builder.getFieldItem(field) + " < " + type_helper.ToString(value)
+}
+
+func (builder *Builder) IsNUll(field string) string {
+	return builder.getFieldItem(field) + " IS NULL"
+}
+
+func (builder *Builder) IsNotNull(field string) string {
+	return builder.getFieldItem(field) + " IS NOT NULL"
 }
 
 func (builder *Builder) Offset(value int) *Builder {
