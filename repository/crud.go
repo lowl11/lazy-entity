cf5f5edc4129c4e48f7f27e59c3778e00b8937f8
lazy_owl <slimsly11@gmail.com>
2023-06-23 21:19:28 +0600

    crud repository interface comments

--- repository/crud.go
+++ repository/crud.go
@@ -12,38 +12,44 @@ import (
 type ICrudRepository[T any, ID repositories.IComparableID] interface {
 	IUniversalRepository[T, ID]
 
-	// ExistByID
+	// ExistByID returns true if record exist
 	ExistByID(id ID) (bool, error)
 
-	// GetAll
+	// GetAll returns all records
 	GetAll() ([]T, error)
 
-	// GetByID
+	// GetByID returns record by ID. 
+	// If the record with such ID does not exist, returns NULL (nil)
 	GetByID(id ID) (*T, error)
 
-	// GetByIdList
+	// GetByIdList returns list of records by array of IDs.
+	// Some of IDs could be not existing, there wouldn't be an error
 	GetByIdList(id []ID) ([]T, error)
 
-	// UpdateByID
+	// UpdateByID updates an record by ID
 	UpdateByID(id ID, entity T) error
 
-	// UpdateByIdTx
+	// UpdateByIdTx the same as UpdateByID but using transaction
 	UpdateByIdTx(tx *sqlx.Tx, id ID, entity T) error
 
-	// DeleteAll
+	// DeleteAll remove all records
 	DeleteAll() error
 
-	// DeleteAllTx
+	// DeleteAllTx the same as DeleteAll but using transaction
 	DeleteAllTx(tx *sqlx.Tx) error
 
-	// DeleteByID
+	// DeleteByID removes record by given ID
 	DeleteByID(id ID) error
 
-	// DeleteByIdTx
+	// DeleteByIdTx the same as DeleteByID but using transaction
 	DeleteByIdTx(tx *sqlx.Tx, id ID) error
 }
 
-func NewCrud[T any, ID repositories.IComparableID](connection *sqlx.DB, tableName string, config repo_config.Crud) ICrudRepository[T, ID] {
+func NewCrud[T any, ID repositories.IComparableID](
+	connection *sqlx.DB,
+	tableName string,
+	config repo_config.Crud,
+) ICrudRepository[T, ID] {
 	// create new instance
 	newRepo := crud_repository.New[T, ID](connection, tableName)
 
